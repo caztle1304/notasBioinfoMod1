8e945275939bf2e746c2b6fbd8fb64780d56e737
caztle1304 <angeldc@lcg.unam.mx>
2021-02-23 12:08:09 -0600

    Primer commit

---
+++ R/01-notas.R
@@ -0,0 +1,40 @@
+library("sessioninfo")
+library("here")
+library("ggplot2")
+
+
+## Hello wordl
+print("Soy Angel")
+
+## Crea diractorio para figuras
+dir.create(here::here("figuras"), showWarnings = FALSE)
+
+pdf(here::here("figuras", "mtcars_gear_vs_mpg.pdf"), useDingbats = FALSE)
+ggplot(mtcars, aes(x = gear, y = mpg)) + geom_boxplot()
+dev.off()
+
+## Para reproducir mi código
+options(width = 120)
+sessioninfo::session_info()
+
+## Para conectar con github
+
+usethis::create_github_token()
+
+## Para pegar token generado en github
+## Se debe pegar el token generado en github, no la contraseña
+
+gitcreds::gitcreds_set()
+
+## Configurar usuario de github
+
+usethis::edit_git_config()
+
+## Inicializar repositorio de git
+
+usethis::use_git()
+
+## Para conectar repositorio local de git con los servidores de GitHub
+
+usethis::use_github()
+
